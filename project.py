1d6febe8f93ad4bc0af6f4a9609b39aa716b7260
Gowtham Kanakasabapathy <gowtham.kanakasabapathy@intel.com>
2021-10-13 11:58:18 -0700

    update config.json

--- project.py
+++ project.py
@@ -2,16 +2,16 @@
 from sharepoint import SharePoint
 
 #i.e - file_dir_path = r'C:\project\report.pdf'
-file_dir_path = r'C:\YOUR-FILE-PATH\FILE-NAME-WITH-EXTENSION'
+file_dir_path = r'C:\Users\gkanakas\OneDrive - Intel Corporation\Documents\GitHub\python-upload2sharepoint\filetoupload.txt'
 
 # this will be the file name that it will be saved in SharePoint as 
-file_name = 'FILE-NAME-WITH-EXTENSION'
+file_name = 'filetoupload.txt'
 
 # The folder in SharePoint that it will be saved under
-folder_name = '2020'
+folder_name = 'test-upload-folder'
 
 # upload file
 SharePoint().upload_file(file_dir_path, file_name, folder_name)
 
 # delete file
-SharePoint().delete_file(file_name, folder_name)
+#SharePoint().delete_file(file_name, folder_name)
